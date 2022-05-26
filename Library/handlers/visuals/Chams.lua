ad6970fecd16f50f9be30c619e0d820d446bfb78
syn5 <88166453+parkersyn5@users.noreply.github.com>
2022-05-26 17:28:43 -0400

    Update Chams.lua

--- Library/handlers/visuals/Chams.lua
+++ Library/handlers/visuals/Chams.lua
@@ -1,3 +1,4 @@
+
 local ChamsVisuals = {}
 
 getgenv().ChamsTeamColors = false
@@ -9,31 +10,31 @@ getgenv().ChamsFillTransparency = 0
 getgenv().ChamsOutlineTransparency = 0
 
 function ChamsVisuals:ToggleChams(boolval)
-        local Players = game:GetService("Players")
-        local RunService = game:GetService("RunService")
-        local function CreateHighlight(Player)
-           if (not Player.Character:FindFirstChild("HighlightCham") and Player ~= Players.LocalPlayer) then
-               local Highlight = Instance.new("Highlight", Player.Character)
-               Highlight.Name = "HighlightCham"
-           end
+    local Players = game:GetService("Players")
+    local RunService = game:GetService("RunService")
+    local function CreateHighlight(Player)
+        if (not Player.Character:FindFirstChild("HighlightCham") and Player ~= Players.LocalPlayer) then
+            local Highlight = Instance.new("Highlight", Player.Character)
+            Highlight.Name = "HighlightCham"
         end
-        RunService.Stepped:Connect(function()
-            for i,v in next, Players:GetPlayers() do
-                CreateHighlight(v)
-                if (v.Character:FindFirstChild("HighlightCham")) then
-                    local Highlight = v.Character.HighlightCham
-                    Highlight.Enabled = getgenv().ChamsEnabled
-                    Highlight.DepthMode = getgenv().ChamsDepthMode
-                    Highlight.FillColor = getgenv().ChamsFillColor
-                    Highlight.OutlineColor = getgenv().ChamsOutlineColor
-                    Highlight.FillTransparency = getgenv().ChamsFillTransparency
-                    Highlight.OutlineTransparency = getgenv().ChamsOutlineTransparency
-                    if getgenv().ChamsTeamColors == true then
-                        Highlight.FillColor = v.TeamColor.Color
-                    end
+    end
+    RunService.Stepped:Connect(function()
+        for i,v in next, Players:GetPlayers() do
+            CreateHighlight(v)
+            if (v.Character:FindFirstChild("HighlightCham")) then
+                local Highlight = v.Character.HighlightCham
+                Highlight.Enabled = getgenv().ChamsEnabled
+                Highlight.DepthMode = getgenv().ChamsDepthMode
+                Highlight.FillColor = getgenv().ChamsFillColor
+                Highlight.OutlineColor = getgenv().ChamsOutlineColor
+                Highlight.FillTransparency = getgenv().ChamsFillTransparency
+                Highlight.OutlineTransparency = getgenv().ChamsOutlineTransparency
+                if getgenv().ChamsTeamColors == true then
+                    Highlight.FillColor = v.TeamColor.Color
                 end
             end
-        end)
+        end
+    end)
 end
 
 return ChamsVisuals
